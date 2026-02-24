fbc229d1d0928305acd0e2336f2b670262c3dd46
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-24 12:09:06 +0200

    roll back package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,21 @@
-// import { defineConfig, globalIgnores } from 'eslint/config';
-// import nextVitals from 'eslint-config-next/core-web-vitals';
-// import nextTs from 'eslint-config-next/typescript';
-// import prettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier';
 
-// const eslintConfig = defineConfig([
-//   ...nextVitals,
-//   ...nextTs,
-//   prettier,
-//   // Override default ignores of eslint-config-next.
-//   globalIgnores([
-//     // Default ignores of eslint-config-next:
-//     '.next/**',
-//     'out/**',
-//     'build/**',
-//     'next-env.d.ts',
-//   ]),
-// ]);
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
 
 // export default eslintConfig;
 
@@ -51,17 +51,17 @@
 
 // export default eslintConfig;
 
-import { FlatCompat } from '@eslint/eslintrc';
+// import { FlatCompat } from '@eslint/eslintrc';
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: import.meta.dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: ['.next/**', 'node_modules/**'],
-  },
-];
+// const eslintConfig = [
+//   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+//   {
+//     ignores: ['.next/**', 'node_modules/**'],
+//   },
+// ];
 
-export default eslintConfig;
+// export default eslintConfig;
